2d2b97180c6519cd8d12ad01d9d3e5034d9a5eb5
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-11-29 23:15:08 +1100

    pretty much works now :)

--- extention/app.js
+++ extention/app.js
@@ -1,30 +1,21 @@
-//boilerplate
-Array.prototype.random = function (){return this[Math.floor((Math.random()*this.length))];}
+chrome.history.search({ text: '' }, results => {
+    
+    var $data = results, 
+        $filteredData = [];
 
-//onload
-promptLogIn()
-    .then((d) => tweet($filteredData.random()));
+    $data.forEach(item => { if( (new URL(item.url)).host == 'www.google.com' && (new URL(item.url)).searchParams.get('q') ) $filteredData.push(item) })
+ 
+    const $historyItem = $filteredData[Math.floor((Math.random() * $filteredData.length))]
+    setTimeout(() => {
 
-function promptLogIn(){
-    return new Promise((resolve, reject) => {
+        const $searchTime = new Date($historyItem.lastVisitTime),
+              $tweetText = `On ${$searchTime.getDate()}/${$searchTime.getMonth()}/${$searchTime.getFullYear()} I searched for "${(new URL($historyItem.url)).searchParams.get('q')}" on Google`
 
-        resolve(true);
+        document.querySelector("#tweetText").innerText = $tweetText;
+        document.querySelector("#twitterShareButton").addEventListener("click", () => chrome.tabs.create({ url: `https://twitter.com/intent/tweet?text=${$tweetText.replace(" ", "%20").replace("#", "%23")}@ryncmrfrd%20%23tweetmyhistory` }) );
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("#result").style.display = "flex";
 
-    })
-}
+    }, 500)
 
-function tweet(txt){
-
-    return;
-
-    var url = `${txt}`
-    new XMLHttpRequest().onreadystatechange = function(){
-                    
-        if (this.readyState == 4 && this.status == 200) {
-            document.querySelector("#result div h1").innerText = txt;
-            document.querySelector("#loading").style.display = "none";
-            document.querySelector("#result").style.display = "block";
-        }
-            
-    }.open("POST", url, true).send();
-}
+})
