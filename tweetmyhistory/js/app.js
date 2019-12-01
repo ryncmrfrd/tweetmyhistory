cd7b92adfaad2e6dee09ebb452a315df1c4fa2b4
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-12-01 22:11:59 +1100

    pretty much finished

--- tweetmyhistory/js/app.js
+++ tweetmyhistory/js/app.js
@@ -12,7 +12,7 @@ chrome.history.search({ text: '' }, results => {
               $tweetText = `On ${$searchTime.getDate()}/${$searchTime.getMonth()}/${$searchTime.getFullYear()} I searched for "${(new URL($historyItem.url)).searchParams.get('q')}" on Google`
 
         document.querySelector("#tweetText").innerText = $tweetText;
-        document.querySelector("#twitterShareButton").addEventListener("click", () => chrome.tabs.create({ url: `https://twitter.com/intent/tweet?text=${$tweetText.replace(" ", "%20").replace("#", "%23")}@ryncmrfrd%20%23tweetmyhistory` }) );
+        document.querySelector("#twitterShareButton").addEventListener("click", () => chrome.tabs.create({ url: `https://twitter.com/intent/tweet?text=${$tweetText.replace(" ", "%20").replace("#", "%23")}%20@ryncmrfrd%20%23tweetmyhistory` }) );
         document.querySelector("#loading").style.display = "none";
         document.querySelector("#result").style.display = "flex";
 
